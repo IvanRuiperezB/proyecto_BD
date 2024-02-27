8ea98b459043bf92b507ffa6b3c92e64e684ac14
Ivan Ruiperez <ivanruiperez.instituto@gmail.com>
2024-02-27 20:56:52 +0100

    Nuevas Funciones

--- funcionesMariaDB.py
+++ funcionesMariaDB.py
@@ -1,20 +1,24 @@
 import sys
 import MySQLdb
 
+def AbreBD():
+    try:
+        db = MySQLdb.connect("localhost","ivan_proyecto","1234","Proyecto" )
+    except MySQLdb.Error as e:
+        print("No puedo conectar a la base de datos:",e)
+        sys.exit(1)
+    return db
 
-try:
-	db = MySQLdb.connect("localhost","ivan_proyecto","1234","Proyecto" )
-except MySQLdb.Error as e:
-	print("No puedo conectar a la base de datos:",e)
-	sys.exit(1)
+def ListaAlumnos(db):
+    sql="SELECT Alumnos.Nombre,Apellido1,Apellido2,COUNT(Tareas.ID) FROM Alumnos,Practicas,Tareas Where Alumnos.DNI = Practicas.DNIAlumno AND Practicas.ID = Tareas.IDPractica GROUP BY Nombre,Apellido1,Apellido2;"
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        registros = cursor.fetchall()
+        for registro in registros:
+            print(registro[0],registro[1],registro[2],registro[3])
+    except:
+        print("Error en la consulta")
 
-sql="SELECT Alumnos.Nombre,Apellido1,Apellido2,COUNT(Tareas.ID) FROM Alumnos,Practicas,Tareas Where Alumnos.DNI = Practicas.DNIAlumno AND Practicas.ID = Tareas.IDPractica GROUP BY Nombre,Apellido1,Apellido2;"
-cursor = db.cursor()
-try:
-   cursor.execute(sql)
-   registros = cursor.fetchall()
-   for registro in registros:
-      print(registro[0],registro[1],registro[2],registro[3])
-except:
-   print("Error en la consulta")
-db.close()
+def CierraBD(db):
+    db.close()
--- programaMariaDB.py
+++ programaMariaDB.py
@@ -1,8 +1,8 @@
 #Iván Ruipérez Benítez
-import sys,MySQLdb
-try:
-	db = MySQLdb.connect("localhost","ivan_proyecto","1234","Proyecto" )
-except MySQLdb.Error as e:
-	print("No puedo conectar a la base de datos:",e)
-	sys.exit(1)
+from funcionesMariaDB import ListaAlumnos,AbreBD,CierraBD
 
+db=AbreBD()
+
+ListaAlumnos(db)
+
+CierraBD(db)
