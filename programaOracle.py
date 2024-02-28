47b297aec0bb842bba3ed451b4047d767e5c0678
Iván Ruipérez <157600550+IvanRuiperezB@users.noreply.github.com>
2024-02-28 13:21:20 +0100

    Pruebas Oracle

--- programaOracle.py
+++ programaOracle.py
@@ -1,1 +1,11 @@
 #Iván Ruipérez Benítez
+from funciones import menu,Oracle_opciones,Oracle_AbreBD,Oracle_CierraBD
+
+db=Oracle_AbreBD()
+
+num=0
+while num != 7:
+    num=menu()
+    Oracle_opciones(num,db)
+
+Oracle_CierraBD(db)
